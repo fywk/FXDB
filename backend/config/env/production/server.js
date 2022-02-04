b52c95abf68e17a30ec5fcb692890dd5bdb58427
Francis Yeong <33077075+fywk@users.noreply.github.com>
2022-02-05 03:53:55 +0800

    Update dependencies

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  url: env("MY_HEROKU_URL"),
+  proxy: true,
   app: {
     keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
   },
